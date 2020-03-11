a7fe5ca891e4f38dc304aaf473030abb093200f6
Helmut Frankenbach <helmut.frankenbach@adidas-group.com>
2020-03-11 08:41:21 +0100

    Chapter 35

---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+
+class SetterInjectedControllerTest {
+	
+	SetterInjectedController controller;
+	
+	@BeforeEach
+	void setUp() {
+		controller = new SetterInjectedController();
+		
+		controller.setGreetingService(new GreetingServiceImpl());
+	}
+
+	@Test
+	void getGreeting() {
+		 System.out.println(controller.getGreeting());
+	}
+
+}
